162d5514145f3797e0b9fb6dd19c948d9ddc82b1
André Keller <andre.keller@vshn.ch>
2018-01-18 14:41:08 +0100

    Change version lookup for packaging.
    The current approach imports the version directly from crmngr. This
    is a problem if the build environment does not have crmngr runtime
    libraries installed and leads to having unnecessary build dependencies.
    The new approach extracts the version from the python file directly,
    allowing to have a single source of version, whitout needing to import
    runtime dependencies during the build process.

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
 """ crmngr setup module. """
 
+from pathlib import Path
+import re
 from setuptools import setup, find_packages
-from crmngr.version import __version__
+
+# Read version from crmngr/version.py
+# we do not import, as this fails if dependencies from install_depends are not
+# available
+with open(Path('./crmngr/version.py')) as version_file:
+    VERSION_REGEX = re.compile(
+        r'^\s*__version__\s*=\s*["\'](?P<version>.*)["\']\s*$'
+    )
+    for line in version_file.readlines():
+        version_match = VERSION_REGEX.match(line)
+        if version_match:
+            __version__ = version_match.groupdict()['version']
+            break
+    else:
+        __version__ = 'unknown'
+        raise Exception('Could not get current version of nacli from '
+                        './nacli/version.py')
 
 setup(
     name='crmngr',
